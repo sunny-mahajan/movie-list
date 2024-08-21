5358d8fcc4f010482387d76308add7d2917e8153
Sunny Kumar <sunny@techformation.co.in>
2024-08-21 21:51:58 +0530

    enable movie update on click from movie list

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useRouter } from "next/router";
 
 function MovieCard({ movie }) {
+  const router = useRouter();
+  const handleUpdateMovieClick = () => {
+    router.push(`/update-movie/${movie.id}`);
+  }
   return (
-    <div className="bg-[#092C39] font-montserrat rounded-2xl w-64 overflow-hidden" data-movie-id={movie.id}>
+    <div onClick={handleUpdateMovieClick} className="bg-[#092C39] font-montserrat rounded-2xl w-64 overflow-hidden cursor-pointer" data-movie-id={movie.id}>
       <img src={movie.poster} alt={movie.title} className="w-full h-80 object-cover rounded-3xl p-2" />
       <div className="px-3 pb-4">
         <h3 className="text-lg font-medium pb-1">{movie.title}</h3>
