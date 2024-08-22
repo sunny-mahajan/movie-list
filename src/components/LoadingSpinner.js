d2fe373a4c9ce02aa7fc6ba6d273d0a40136cfee
Sunny Kumar <sunny@techformation.co.in>
2024-08-22 09:30:29 +0530

    change loader color

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -4,7 +4,7 @@ const LoadingSpinner = () => {
   return (
     <div className="flex items-center justify-center space-x-2 absolute">
       <div
-        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        className="animate-spin inline-block w-8 h-8 border-4 rounded-full border-t-[#092C39] border-r-[#092C39] border-b-[#224957] border-l-[#224957]"
         role="status"
       >
         <span className="visually-hidden"></span>
